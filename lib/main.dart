3252b93562f61e5eb407c90e15bc1e7b9049d35e
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-07 20:28:19 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointment_project/view/screen/payment/payment_gateway.dart';
 import 'package:doctor_appointment_project/view/screen/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: PaymentGateway(),
     );
   }
 }
---
+++ lib/view/screen/payment/payment_gateway.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PaymentGateway extends StatelessWidget {
+  const PaymentGateway({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+
+          children: [
+            Column(
+              children: [
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
